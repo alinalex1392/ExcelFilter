a1b2a9d0879b380ca47f112901db47974cb3e3a2
alin.baicu <alinalex1392@gmail.com>
2019-01-20 16:01:15 +0200

    Adding Dummy commits to master

--- src/main/java/com/samsung/excel/util/ExcelUtil.java
+++ src/main/java/com/samsung/excel/util/ExcelUtil.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ExcelUtil {
-//    comment
+//    comment1
 
     private static final List<String> headersWanted = ExcelHeadersEnum.getAllHeadersName();
 
